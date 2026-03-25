d0376316a78db810c70fc75b9328faf8ef464f66
juanj <juajocervantes16@gmail.com>
2026-03-25 17:13:45 -0600

    Commit automatico

--- src/components/HeaderBar/HeaderBar.tsx
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -489,6 +489,7 @@ export function HeaderBar({
             >
               <Menu.Item 
                 leftSection={<IconUser size={16} />}
+                onClick={() => navigate('/profile')}
                 style={{ color: "#FFFFFF" }}
               >
                 Profile
